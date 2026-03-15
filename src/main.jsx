b0b92df5f8ca006beb8c404974eab723d5824336
ShovonTheBarua <shovonbarua0186@gmail.com>
2026-03-15 09:42:40 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./Component/layout/RootLayout.jsx";
+import Home from "./Component/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        path: "home",
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
